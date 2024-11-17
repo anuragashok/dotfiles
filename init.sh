461a082ec7a818f053de846478dccd8cee43b946
Anurag <anurag@anuragashok.com>
2024-11-17 23:38:39 +0800

    update init script

--- init.sh
+++ init.sh
@@ -1,3 +1,4 @@
 export GITHUB_USERNAME=anuragashok && \
+export PATH="$PATH:$HOME/.local/bin" && \
 curl https://bws.bitwarden.com/install | sh && \
 sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply $GITHUB_USERNAME
